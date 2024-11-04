c9e94786fbc72c96b6f3d604b3e9ed49afce83a2
manaseeeee <159604831+manaseeeee@users.noreply.github.com>
2024-11-04 15:31:24 +0530

    navivebayes

--- multinominalnb.py
+++ multinominalnb.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 # Step 1: Load the dataset
