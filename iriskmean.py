112340bb60b4069229aa3ea29869f99e390220db
chinmayee <159604831+manaseeeee@users.noreply.github.com>
2024-11-06 02:24:25 +0530

    Create iriskmean.py

---
+++ iriskmean.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+X = iris.data  
+
+plt.scatter(X[:, 0], X[:, 1], c='blue', marker='o')
+plt.title('Scatter Plot of Iris Data (Sepal Length vs Sepal Width)')
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.show()
+
+inertias = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, random_state=42)
+    kmeans.fit(X)  # Use the features, not the labels
+    inertias.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), inertias, marker='o')
+plt.title('Elbow Method for Optimal k')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.show()
+
+optimal_k = 3  
+kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+kmeans.fit(X)
+
+plt.scatter(X[:, 0], X[:, 1], c=kmeans.labels_, cmap='viridis', marker='o')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x', s=200, label='Centroids')
+plt.title('K-means Clustering of Iris Dataset (Sepal Length vs Sepal Width)')
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.legend()
+plt.show()
